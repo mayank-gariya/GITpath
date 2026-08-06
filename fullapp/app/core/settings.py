b861edff3c5eed78255342ca5f3ba381c03aaeb4
mayank-ml_dev <albert.creates85@gmail.com>
2026-08-06 17:36:40 +0530

    Update settings.py

--- fullapp/app/core/settings.py
+++ fullapp/app/core/settings.py
@@ -1,16 +1,19 @@
+import streamlit as st
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     APP_NAME: str = "GitPath"
     DEBUG: bool = True
 
-    GITHUB_TOKEN: str
+    # Pull from st.secrets if env variable is not present
+    GITHUB_TOKEN: str = st.secrets.get("GITHUB_TOKEN", "")
 
     REQUEST_TIMEOUT: int = 10
 
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
+        extra="ignore"
     )
 
 settings = Settings()
